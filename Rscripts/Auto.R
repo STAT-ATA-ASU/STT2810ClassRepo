c2460c54b95cea60df17b6ca1ed9826795161564
Alan Arnholt <arnholtat@appstate.edu>
2015-11-04 11:11:49 -0500

    rubric updates

--- Rscripts/Auto.R
+++ Rscripts/Auto.R
@@ -1,15 +1,14 @@
-# Alan Arnholt
-# 11/03/15
-# Auto from ISLR
 library(ISLR)
 ?Auto
 str(Auto)
+
 library(ggplot2)
-ggplot(data = Auto, aes(x = factor(cylinders), y = mpg)) +
-  geom_boxplot()
-ggplot(data = Auto, aes(x = horsepower, y = weight)) + 
-  geom_point()
-ggplot(data = Auto, aes(x = horsepower, y = displacement)) + 
-  geom_point()
-ggplot(data = Auto, aes(x = displacement, y = acceleration, color = factor(origin))) + 
-  geom_point() + geom_smooth(method = lm)
+ggplot(data = Auto, aes( x = weight, y = horsepower)) +
+  geom_point() + 
+  geom_smooth(method = lm)
+
+mod <- lm(horsepower ~ weight, data = Auto)
+summary(mod)
+
+cor(Auto$weight, Auto$horsepower)
+cor.test(Auto$weight, Auto$horsepower)
